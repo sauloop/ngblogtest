73492ad58e9bb130075249e89146daf7318fc05d
Pablo Giraldo <pablogirald@yahoo.es>
2020-07-14 18:58:07 +0200

    Test

--- src/main/java/info/pablogiraldo/ngblogtest/NgblogtestApplication.java
+++ src/main/java/info/pablogiraldo/ngblogtest/NgblogtestApplication.java
@@ -3,9 +3,7 @@ package info.pablogiraldo.ngblogtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import info.pablogiraldo.ngblogtest.config.SpringSecurityConfig;
-
-@SpringBootApplication(exclude = SpringSecurityConfig.class)
+@SpringBootApplication
 public class NgblogtestApplication {
 
 	public static void main(String[] args) {
